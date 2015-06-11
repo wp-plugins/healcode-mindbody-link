12e6fa328bd5710d643bf472b3f06a978325f084
healcode_wordpress <healcode_wordpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-11 02:52:24 +0000

    tagging version 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/healcode-mindbody-link/tags/1.1.1@1178694 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/healcode_mb_link.php
@@ -0,0 +1,251 @@
+<?php
+/*
+Plugin Name: HealCode MINDBODY Link
+Plugin URI: https://wordpress.org/plugins/healcode-mindbody-link/
+Description: Add a HealCode Link to your WordPress main navigation menu.
+Version: 1.1.0
+Author: HealCode
+Author URI: http://www.healcode.com/
+Text Domain: healcode-mb-link
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+// ***********************************************************
+// Adding the Dashboard submenu for HealCode MINDBODY Links
+// ***********************************************************
+
+function add_html_menu_item() {
+    add_options_page(
+            'HealCode MINDBODY Link Settings',
+            'HC Login Link',
+            'manage_options',
+            'html-menu-healcode',
+            'menu_html_options'
+        );
+}
+
+add_action('admin_menu', 'add_html_menu_item');
+
+
+// *********************************************
+// HTML content of the Options page
+// *********************************************
+
+// hc_settings_page() displays the page content for the Test settings submenu
+function menu_html_options() {
+
+    //must check that the user has the required capability 
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page. Please contact the Administrator of this website.') );
+    }
+
+    // variables for the field and option names 
+    $opt_name = 'hc_service_link';
+    $hidden_field_name = 'hc_submit_hidden';
+    $data_field_name = 'hc_service_link';
+
+    // Read in existing option value from database
+    $opt_val = get_option( $opt_name );
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+        // Read their posted value
+        $opt_val = stripslashes($_POST[ $data_field_name ]);
+
+        // Save the posted value in the database
+        update_option( $opt_name, $opt_val );
+
+        // Put an settings updated message on the screen
+
+?>
+<div class="updated"><p><strong><?php _e('settings saved.', 'menu-test' ); ?></strong></p></div>
+<?php
+
+    }
+
+// *********************************************
+// The HTML and form of the settings page
+// *********************************************
+
+    echo '<div class="wrap">'; ?>
+    
+    <!-- Header Links to our About page in WordPress and HealCode -->
+
+<div style="margin-top: 10px">
+<table style="float:right; ">
+    <tr>
+        
+        <td style="float:right;">
+            <a class="hc_header_link" style="margin-left:8px;" target="_blank" href="https://wordpress.org/plugins/healcode-mindbody-link/">About</a>
+        </td>
+        <td style="float:right;">
+            <a class="hc_header_link" target="_blank" href="http://www.healcode.com">HealCode</a>
+        </td>
+
+    </tr>
+</table>
+</div>
+
+<div style="clear: both"></div>
+
+<?php echo "<h2>" . __( 'HealCode MINDBODY Link Settings', 'menu-test' ) . "</h2>"; ?>
+
+<p>You will insert your HealCode Link code in the form below. Once saved, the HealCode Link will become a list item in your menu.</p>
+<p><em>You will navigate to the Links page in your HealCode account to find the necessary Javascript code. You can find directions for using this plugin <a href="https://healcode.zendesk.com/hc/en-us/articles/203681304">here</a> and directions to configuring Service Links <a href="https://healcode.zendesk.com/hc/en-us/articles/203040594-Configuring-a-Service-Link-e-g-a-Buy-Now-link-or-My-Account-link-for-deployment">here</a>. For questions, contact support <a href="http://www.healcode.com/tech_support/">here</a>.</em></p><hr />
+
+<!-- ----------------------------------------------- -->
+
+<?php 
+    
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        if (empty($_POST["hc_service_link"])) {
+            $opt_val = "";
+        } else {
+            $opt_val = test_input($_POST["hc_service_link"]);
+        }
+    }
+    
+?>
+
+
+<!-- ----------------------------------------------- -->
+
+<form name="form1" method="post" action="">
+<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+
+<p><?php _e("Your HealCode Link Code:", 'menu-test' ); ?></p>
+<textarea name="<?php echo $data_field_name; ?>" rows="5" cols="100"><?php echo $opt_val;?></textarea>    
+<hr />
+
+</div>
+
+<p class="submit">
+<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+</p>
+
+</form>
+
+<h2 style="margin:0.5em;">Example</h2>
+<img src="http://i.imgur.com/n7ZN4gU.png" style="width:60%;">
+
+<!-- ----------------------------------------------- -->
+
+<!-- Footer of page -->
+
+<div style="clear: both;"></div>
+  
+<p></p>
+
+<div style="width: 100%">
+    <div class="hc_feedback_links">
+        
+<!-- Footer Links, including tech support and social media -->
+
+        <a target="_blank" href="https://healcode.zendesk.com/hc/en-us/articles/203681304" class="instruct">Instructions</a>
+        <a target="_blank" href="http://www.healcode.com/tech_support" class="">Tech Support</a>
+         <a target="_blank" href="https://manager.healcode.com/users/sign_in" class="">Login to HealCode</a>
+        <a target="_blank" href="https://www.facebook.com/HealCode" class="">Like us on Facebook</a> 
+        <a target="_blank" href="https://twitter.com/healcode" class="">Follow us on Twitter</a>
+        
+    </div>
+    <p></p>
+</div>
+
+<p style="clear: both;"></p>
+
+<!-- Styling the submenu -->
+
+<style>
+    
+    a {
+        text-decoration: none;
+    }
+
+    label{
+        cursor:default;
+    }
+    
+    .wrap, p.submit {
+        margin-left:8px !important;
+    }
+    
+    .hc_feedback_links{
+        background: #333; /* Old browsers */
+        border: 0px solid #333;
+        border-radius:3px;
+        width: 96%;    
+        height:30px;
+        padding: 10px;
+        font-weight: bold;
+        line-height: 30px;
+        margin-left:8px;
+        text-align:center;
+    }
+    
+        
+    .hc_feedback_links a{
+        text-decoration: none;
+        color:#fff;
+        padding:0px 30px;
+        vertical-align:center;
+        text-transform:uppercase;
+    }
+
+    a.instruct {
+        color:#a4bb6b;
+    }
+
+    a.hc_header_link {
+    color:#f68f1e;
+    margin-right:15px;
+    font-weight:bold;
+    }
+
+   .hc-short thead tr th {
+    background-color:#D2D2D2;
+    }
+    
+</style>
+
+<?php
+}
+
+// *********************************************
+// Adding the HTML list item to the menu
+// *********************************************
+
+add_filter('wp_nav_menu_items','add_HTML_li', 10, 2);
+function add_HTML_li($item, $args) {
+
+    $opt_val = get_option( 'hc_service_link' );
+    // $items .= '<li>' . $searchform . '</li>';
+	$item .= '<script src="https://widgets.healcode.com/javascripts/healcode.js" type="text/javascript"></script>' . '<li>' . stripslashes($opt_val) . '</li>';
+
+    return $item;
+} ?>
---
+++ trunk/readme.txt
@@ -0,0 +1,130 @@
+=== HealCode MINDBODY Link ===
+Contributors: healcode_wordpress 
+Donate link: http://healcode.com/
+Tags: HealCode, MINDBODY, add HealCode, insert HealCode Link, insert Link, add Links, add HealCode Link, add HealCode Links, add Login, MINDBODY Login, MBO, MINDBODY API, MINDBODY website integration, MINDBODY website, MINDBODY Widgets, pear, pilates, schedule, HealCode Links, soap, yoga, timetable
+Requires at least: 3.0
+Tested up to: 4.2.2
+Stable tag: 1.1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+With our Login | Register Link, your clients will be able to stay on your website to manage their accounts and see their schedule. 
+
+
+
+== Description ==
+
+**Now developers and non-developers alike can add a HealCode Link, specifically the Login | Register link, to the main navigation menu!** 
+
+This plugin lets you add a Login | Register link to the right-most end of your navigation menu. This Link (also called the My Account link) will pull up the HealCode Consumer Cart, which allows your clients to login to their MINDBODY accounts from your website and to create new MINDBODY accounts from your website.
+
+HealCode syncs with your MINDBODY account and can display MINDBODY information including classes, enrollments, appointments, staff biographies, class/enrollment descriptions, and more. See our other WordPress plugin, [HealCode MINDBODY Widget](https://wordpress.org/plugins/healcode-mindbody-widget/ "HealCode MINDBODY Widget").
+
+*Keep in mind that if you want to add Widgets or Buy Now links to a Pricing page, you will need to either use our other plugin, [HealCode MINDBODY Widget](https://wordpress.org/plugins/healcode-mindbody-widget/), or the traditional installation method [here](https://healcode.zendesk.com/hc/en-us/articles/203040594-Configuring-a-Service-Link-e-g-a-Buy-Now-link-or-My-Account-link-for-deployment).*
+
+**NOTE** You will need a HealCode account to use this plugin. Visit [HealCode](http://www.healcode.com/ "HealCode") to sign up! If you are a developer, you can learn more about our Developer Partner program [here](http://www.healcode.com/developer-signup/).
+
+= Plugin Features =
+* Ability to add one HealCode Link to the end of the navigation menu(s)
+* Link can be targeted with CSS (styling code)
+* Link text can be adjusted or replaced with an image
+
+= Buy Now, Login | Register, and Cart link Features =
+([see this page for a description of our Widgets and Consumer Cart](https://healcode.zendesk.com/hc/en-us/articles/202867484--What-are-Service-Links-i-e-Buy-Now-links-My-Account-links-and-Cart-links-))
+
+HealCode Links are links you put on your website to provide the following, specific functions:
+
+The **Login | Register link** provides a client the ability to log in and see their account information. Once in the My Account area, they can view their contact info, update their credit card, view their schedule, view their purchase history or view their account balances. When the Login | Register Link is first clicked, it brings up the login screen of the HealCode Consumer Cart.
+
+The **My Cart link** provides a client the ability to view their cart and any items that are in it. See: [HealCode MINDBODY Widget](https://healcode.zendesk.com/hc/en-us/articles/202867484--What-are-Service-Links-i-e-Buy-Now-links-My-Account-links-and-Cart-links-)
+
+A **Buy Now link** provides a client the ability to purchase a "Pricing Option" (as that term is used in MINDBODY) directly from your website. See: [HealCode MINDBODY Widget](https://healcode.zendesk.com/hc/en-us/articles/202867484--What-are-Service-Links-i-e-Buy-Now-links-My-Account-links-and-Cart-links-)
+
+
+
+== Installation ==
+
+1. Upload all the files and folders in the `healcode-mindbody-link` folder to the `/wp-content/plugins/` directory
+2. In the admin panel under "Plugins" activate HealCode MINDBODY Link
+
+
+
+== Frequently Asked Questions ==
+
+= What are HealCode Links? What is a Login | Register Link? =
+
+HealCode Links are links you put on your website to provide the following, specific functions:
+
+The Login | Register link provides a client the ability to login and see their account information. Once in the My Account area, they can view their contact info, update their credit card, view their schedule, view their purchase history or view their account balances. When the Login | Register link is first clicked, it brings up the login screen of the HealCode Consumer Cart.
+
+The Cart link provides a client the ability to view their cart and any items that are in it.
+
+The Buy Now link provides a client the ability to purchase a "Pricing Option" (as that term is used in MINDBODY) directly from your website. Learn about what Pricing Options are supported [here](https://healcode.zendesk.com/hc/en-us/articles/202985440--What-is-the-Consumer-Cart-and-known-issues-for-it "here").
+
+[Sign up for HealCode!](http://www.healcode.com/sign-up/ "Sign up for HealCode!")
+
+= How do I add a HealCode Login | Register Link to the navigation menu ? =
+
+See our [HealCode MINDBODY Link Plugin FAQ](https://healcode.zendesk.com/hc/en-us/articles/203681304)
+
+= How many Links can I add to the navigation menu ? =
+
+At this time, the HealCode MINDBODY Link plugin can only add one HealCode Link to the end of the main menu.
+
+= I have two menus (or more) and the Link is appearing in all of them. What should I do ? =
+
+[Contact HealCode support](http://www.healcode.com/tech_support/) so that we can provide you with instructions for removing the extra link. If you are comfortable with CSS, you can use CSS to hide the extra link(s).
+
+= Where do I find my HealCode Link code ? =
+
+You can find the code for each Link in each in the HealCode Manager. You can login [here](https://manager.healcode.com/users/sign_in "here"). 
+
+= HealCode MINDOBDY Link is not working properly. =
+
+Please check the WordPress version you are using. Make sure it meets the minimum version recommended by us. Make sure all files of the `healcode_mindbody_link` plugin are uploaded to the folder `wp-content/plugins/`
+
+= I have another plugin installed for my navigation menu, and HealCode MINDBODY Link isn’t working now . =
+
+Unfortunately, we cannot guarantee that our plugin will work with other plugins, especially plugins that are made specifically for the navigation menu.
+
+More questions ? [Contact HealCode Support.](http://www.healcode.com/tech_support/)
+
+
+
+== Screenshots ==
+
+1. The HealCode Links screen in HealCode where you will copy the code for your link
+2. The settings screen where our demo HealCode Link code has been added to the HealCode Link code text box
+3. The result, the Login | Register link has been added to the end of our main navigation menu
+4. When clicked, your Login | Register link will pull up the HealCode Consumer Cart
+
+
+== Requirements ==
+
+* WordPress 3.0+
+* PHP 5+
+* [Activated HealCode account](http://www.healcode.com/ "Activated HealCode account") 
+* [Activated MINDBODY account](https://www.mindbodyonline.com/ "Activated MINDBODY account")
+
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Compatibility with WordPress 4.1
+= 1.0.1 =
+* Updating readme lingo
+= 1.1.0 =
+* Fixed a bug in the textarea field and added the script tag to automatically be entered into the nav with the Link code
+= 1.1.1 =
+* Fixed a bug in the textarea field and added the script tag to automatically be entered into the nav with the Link code
+
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* First version
+= 1.1.0 =
+* Fixed a bug in the textarea field and added the script tag to automatically be entered into the nav with the Link code
+
+
